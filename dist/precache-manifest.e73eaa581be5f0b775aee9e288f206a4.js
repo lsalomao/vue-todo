80cc6cd097e2a6f300c3f0257c3ad7a48909462f
Leandro Salomao <ledsalomao@gmail.com>
2021-11-23 23:07:23 -0300

    second commit

---
+++ dist/precache-manifest.e73eaa581be5f0b775aee9e288f206a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce062b51add34cd05ed6",
+    "url": "/css/chunk-vendors.51b2e17d.css"
+  },
+  {
+    "revision": "62edcfbbab9d778ea17d8b5218626236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d59c92c22bf4ab184eb",
+    "url": "/js/app.8febbacd.js"
+  },
+  {
+    "revision": "e4067fb9be0fdbeb4892",
+    "url": "/js/chunk-2d22d746.2e4c0305.js"
+  },
+  {
+    "revision": "ce062b51add34cd05ed6",
+    "url": "/js/chunk-vendors.aa978918.js"
+  },
+  {
+    "revision": "fbafddbca0454e2cf83c4640f9391b35",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
