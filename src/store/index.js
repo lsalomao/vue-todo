f7e617e60b31207ef366379fb89c7a473ec47147
Leandro Salomao <ledsalomao@gmail.com>
2021-11-23 23:04:02 -0300

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,34 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    tarefas: [
+      { id: 1, titulo: "Ir ao mercado", concluido: false },
+      { id: 2, titulo: "Comprar PC", concluido: true },
+      { id: 3, titulo: "Trabalhar", concluido: false },
+    ],
   },
   mutations: {
+    AddTarefa(state, titulo) {
+      if (titulo) {
+        state.tarefas.push({
+          id: new Date().getTime(),
+          titulo,
+          concluido: false,
+        });
+      }
+    },
+    removeTarefa(state, id)
+    {
+      state.tarefas = state.tarefas.filter(tarefa => tarefa.id != id);
+      
+      
+    },
+
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
